92e0cda6476fca1a150ad95bb26035fdff5807f0
Parsa Joneidi <parsajoneidy@gmailc.om>
2021-07-18 07:11:31 -0700

    reading list controller code cleaned

--- app/Controllers/Http/ReadingListController.js
+++ app/Controllers/Http/ReadingListController.js
@@ -5,27 +5,32 @@ const Post = use("App/Models/Post")
 class ReadingListController {
     async load({request, response, auth}){ //Loading saved posts by user
         try{
+
             const user = await auth.getUser()
+
             try{
-            const posts_ids = user && user["reading_list"] && user["reading_list"] != "" ? user["reading_list"].split(",") : []
-            for(let i = 0; i < posts_ids.length; i++){
-                posts_ids[i] = parseInt(posts_ids[i])
-            }
-            const posts = await Post.query().whereIn("id",posts_ids).fetch()
-            return response.status(200).json({
-                massage: "Posts loaded succefully",
-                posts: posts                
-            })
+
+                const posts_ids = user && user.reading_list && user.reading_list != "" ? user.reading_list.split(",") : []
+
+                for(let i = 0; i < posts_ids.length; i++){
+
+                    posts_ids[i] = parseInt(posts_ids[i])
+
+                }
+
+                const posts = await Post.query().whereIn("id",posts_ids).fetch()
+                return response.status(200).json({massage: "Posts loaded succefully",posts: posts})
+
             }catch(e){
-                return response.status(500).json({
-                    massage: "Error loading posts",
-                    error: e
-                })
+
+                return response.status(500).json({massage: "Error loading posts",error: e})
+
             }
+
         }catch(e){
-            return response.status(401).json({
-                massage: "User is not authenticated"
-            })
+
+            return response.status(401).json({massage: "User is not authenticated"})
+
         }
     }
 
@@ -34,48 +39,56 @@ class ReadingListController {
             const user = await auth.getUser()
             const query = request.get()
             const post = query.id
+
             try{
+
                 await Post.findOrFail(post)
+
             }catch(e){
-                return response.status(404).json({
-                    massage: "Post doesn't exist"
-                })
+
+                return response.status(404).json({massage: "Post doesn't exist"})
+
             }
-            const posts_ids = user && user["reading_list"] && user["reading_list"] != "" ? user["reading_list"].split(",") : []
+
+            const posts_ids = user && user.reading_list && user.reading_list != "" ? user.reading_list.split(",") : []
+
             if(!posts_ids.includes(post)){
                 posts_ids.push(post)
-                user["reading_list"] = posts_ids.toString()
+                user.reading_list = posts_ids.toString()
+
                 try{
+
                     await user.save()
-                    return response.status(200).json({
-                        massag: "Post saved succesfully"
-                    })
+                    return response.status(200).json({massag: "Post saved succesfully"})
+
                 }catch(e){
-                    return response.status(500).json({
-                        massage: "Error saving post",
-                        error: e
-                    })
+
+                    return response.status(500).json({massage: "Error saving post",error: e})
+
                 }
+
             }else{
+
                 posts_ids.splice(posts_ids.indexOf(post),1)
-                user["reading_list"] = posts_ids.toString()
+                user.reading_list = posts_ids.toString()
+
                 try{
+
                     await user.save()
-                    return response.status(200).json({
-                        massag: "Post unsaved succesfully"
-                    })
+                    return response.status(200).json({massag: "Post unsaved succesfully"})
+
                 }catch(e){
-                    return response.status(500).json({
-                        massage: "Error unsaving post",
-                        error: e
-                    })
+
+                    return response.status(500).json({massage: "Error unsaving post",error: e})
+
                 }
+
             }
             
         }catch(e){
-            return response.status(401).json({
-                massage: "User is not authenticated"
-            })
+
+            return response.status(401).json({massage: "User is not authenticated"})
+
         }
 
     }
