87447535f8f47782109dc6c833b315d89b494b13
Parsa Joneidi <parsajoneidy@gmailc.om>
2021-07-18 05:49:16 -0700

    manager controller code cleaned

--- app/Controllers/Http/ManagerController.js
+++ app/Controllers/Http/ManagerController.js
@@ -4,74 +4,72 @@ const User = use("App/Models/User")
 class ManagerController {
     async deleteUser({response, params}){
         const user = await User.find(params.id)
-        if(!user){
-            return response.status(404).json({massage: "User not found"})
-        }
-        if(user.role == "Manager"){
-            return response.status(401).json({massage: "You can not kick a manager"})
-        }
+
+        if(!user){return response.status(404).json({massage: "User not found"})}
+
+        if(user.role == "Manager"){return response.status(401).json({massage: "You can not kick a manager"})}
+
         try{
+
             await user.delete()
-            return response.status(200).json({
-                massage: "User deleted"
-            })
-            }catch(e){
-            return response.status(500).json({
-                massage: "Error deleting user",
-                error: e
-            })
-            }
+            return response.status(200).json({massage: "User deleted"})
+
+        }catch(e){
+
+            return response.status(500).json({massage: "Error deleting user",error: e})
+
+        }
+
     }
     async allUsers({response}){
+
         try{
+
             const users = await User.query().whereNot("role","Manager").fetch()
-            return response.status(200).json({
-                massage: "Users loaded successfully",
-                users: users
-            })
+            return response.status(200).json({massage: "Users loaded successfully",users: users})
+
         }catch(e){
-            return response.status(500).json({
-                massage: "Error loading users",
-                error: e
-            })
+
+            return response.status(500).json({massage: "Error loading users",error: e})
+
         }
 
     }
     async promote({request, params, response}){
+
         const user = await User.find(params.id)
-        if(!user){
-            return response.status(404).json({massage: "User not found"})
-        }
+        if(!user){return response.status(404).json({massage: "User not found"})}
+
         try{
-        user.role = "Admin"
-        await user.save()
-        return response.status(200).json({
-            massage: "User promoted successfully"
-        })
+
+            user.role = "Admin"
+            await user.save()
+            return response.status(200).json({massage: "User promoted successfully"})
+
         }catch(e){
-            return response.status(500).json({
-                massage: "Error promoting user",
-                error: e
-            })
+
+            return response.status(500).json({massage: "Error promoting user",error: e})
+
         }
+
     }
     async demote({params, response}){
         const user = await User.find(params.id)
-        if(!user){
-            return response.status(404).json({massage: "User not found"})
-        }
+
+        if(!user){return response.status(404).json({massage: "User not found"})}
+
         try{
+
             user.role = "User" 
             await user.save()
-            return response.status(200).json({
-                massage: "User demoted"
-            })
-            }catch(e){
-                return response.status(500).json({
-                    massage: "Error deomoting user",
-                    error: e
-                })
-            }
+            return response.status(200).json({massage: "User demoted"})
+
+        }catch(e){
+
+            return response.status(500).json({massage: "Error deomoting user",error: e})
+
+        }
+        
     }
 }
 
