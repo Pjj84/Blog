83ec5d1f8ba686ddc2488b3ef77c4687277b9576
Parsa Joneidi <parsajoneidy@gmailc.om>
2021-07-18 05:46:11 -0700

    comment controller code cleaned

--- app/Controllers/Http/CommentController.js
+++ app/Controllers/Http/CommentController.js
@@ -11,189 +11,205 @@ class CommentController {
     async create({request, response, auth, params}){
         const comment = new Comment
         try{
+
             const user = await auth.getUser()
-            comment["status"] = "Approved"
-            comment['user_id'] = user.id
-            comment["user_fullname"] = user.fullname
-            user["comments_count"] = user["comments_count"] + 1
+            comment.status = "Approved"
+            comment.user_id = user.id
+            comment.user_fullname = user.fullname
+            user.comments_count++
+
         }catch(e){
-            comment["status"] = "Pending"
-            comment["user_id"] = null
-            if(request.input("fullname")){
-            comment["user_fullname"] = request.input("fullname")
-            }else{
-                return response.status(422).json({massage: "Field user fullname cannot be empty"})
-            }
+
+            comment.status = "Pending"
+            comment.user_id = null
+
+            if(request.input("fullname")){comment.user_fullname = request.input("fullname")}
+            else{return response.status(422).json({massage: "Field user fullname cannot be empty"})}
+
         }
-        const post = await Post.find(params["post_id"])
-        if(!post){
-            return response.status(404).json({massage: "Post not found"})
-        }
-        comment['post_id'] = params['post_id'] //The id of the post
-        if(params.name){
-        comment["in_reply_to"] = params.name
-        }
+        const post = await Post.find(params.post_id)
+
+        if(!post){return response.status(404).json({massage: "Post not found"})}
+
+        comment.post_id = params.post_id
+
+        if(params.name){comment.in_reply_to = params.name}
+
         if(!request.input("text")){return response.status(422).json({massage: "Field text cannot be empty"})}
-            comment.text = request.input('text')
-        comment["reply_to"] = params["comment_id"] || null
+
+        comment.text = request.input('text')
+        comment.reply_to = params.comment_id || null
         comment.replies = ""
+
         try{
+
             await comment.save()
+
         }catch(e){
-            return response.status(500).json({
-                massage: "Error creating comment",
-                error: e
-            })
+
+            return response.status(500).json({massage: "Error creating comment",error: e })
+
         }
-        if(params['comment_id']){
-            const replied_comment = await Comment.find(params['comment_id'])
-            if(replied_comment["reply_to"]){
-                return response.status(400).json({massage: "Cannot reply to a replying comment"})
-            }
-            if(!replied_comment){
-                return response.status(404).json({massage: "The comment you are trying to reply does not exist"})
-            }
+
+        if(params.comment_id){
+            const replied_comment = await Comment.find(params.comment_id)
+            
+            if(replied_comment.reply_to){return response.status(400).json({massage: "Cannot reply to a replying comment"})}
+
+            if(!replied_comment){return response.status(404).json({massage: "The comment you are trying to reply does not exist"})}
+
             const array = replied_comment.replies ? replied_comment.replies.split(",") : [] 
             array.push(comment.id)
             replied_comment.replies = array.toString().substring(0,array.toString().length + 1)
+
             try{
+
                 await replied_comment.save()
-                return response.status(200).json({
-                    massage: "Comment and relpies saved successfully"
-                })
+                return response.status(200).json({massage: "Comment and relpies saved successfully"})
+
             }catch(e){
-                return response.status(500).json({
-                    massage: "Error saving replies",
-                    error: e
-                })
+
+                return response.status(500).json({massage: "Error saving replies",error: e})
             }
         }else{
-            return response.status(200).json({
-                massage: "Comment created succefully"
-            })
-        }
 
+            return response.status(200).json({massage: "Comment created succefully"})
+
+        }
     }
     async edit({auth, request, response, params}){
         const comment = await Comment.find(params.id)
-        if(!comment){
-            return response.status(404).json({massage: "Comment not found"})
-        }
+
+        if(!comment){return response.status(404).json({massage: "Comment not found"})}
+
         try{
-        const user = await auth.getUser()
-            if(comment["user_id"] == null){
-                if(user.role != "Admin" && user.role != "Manager"){
-                    return response.status(401).json({message: "Only manager and admin can edit this comment"})
-                }
+
+            const user = await auth.getUser()
+
+            if(comment.user_id == null){
+
+                if(user.role != "Admin" && user.role != "Manager"){return response.status(401).json({message: "Only manager and admin can edit this comment"})}
+
             }else{
-                if(comment["user_id"] != user.id && user.role != "Admin" && user.role != "Manager"){
-                    return response.status(401).json({
-                        massage: "Only manager, admin and creator of the comment can edit this comment"
-                    })
-                }
+
+                if(comment.user_id != user.id && user.role != "Admin" && user.role != "Manager"){return response.status(401).json({massage: "Only manager, admin and creator of the comment can edit this comment"})}
+
             }
-            comment["user_id"] = user.id
+
+            comment.user_id = user.id
+
         }catch(e){
-            comment["status"] = "Pending"
-            comment["user_id"] = null
-            if(request.input("fullname")){
-                comment["foreign_name"] = request.input("fullname")
-                }else{
-                    return response.status(422).json({massage: "Fied name cannot be empty"})
-                }
+
+            comment.status = "Pending"
+            comment.user_id = null
+
+            if(request.input("fullname")){comment.foreign_name = request.input("fullname")}
+            else{return response.status(422).json({massage: "Fied name cannot be empty"})}
+
         }
-        if(!request.input("text")){
-            return response.status(422).json({message: "Comment text can not be empty"})
-        }
+
+        if(!request.input("text")){return response.status(422).json({message: "Comment text can not be empty"})}
+
         comment.text = request.input('text')
+
         try{
+
             comment.save()
-            return response.status(200).json({
-                massage: "Comment eidted successfully",
-                comment: comment
-            })
+            return response.status(200).json({massage: "Comment eidted successfully",comment: comment})
+
         }catch(e){
-            return response.status(500).json({
-                massage: "Error editing comment",
-                error: e
-            })
+
+            return response.status(500).json({massage: "Error editing comment",error: e})
+
         }
     }
     async delete({params, response, auth}){
         const comment = await Comment.find(params.id)
-        if(!comment){
-            return response.status(404).json({masage: "Comment not found"})
-        }
+
+        if(!comment){return response.status(404).json({masage: "Comment not found"})}
+
         try{
-        const user = await auth.getUser()
-        if(comment["user_id"] == null){
-            if(user.role != "Admin" && user.role != "Manager"){
-                return response.status(401).json({message: "Only manager and admin can delete this comment"})
+
+            const user = await auth.getUser()
+
+            if(comment.user_id == null){
+
+                if(user.role != "Admin" && user.role != "Manager"){return response.status(401).json({message: "Only manager and admin can delete this comment"})}
+
             }
-        }
-        if(comment["user_id"] != user.id && user.role != "Admin" && user.role != "Manager"){
-            return response.status(401).json({
-                massage: "Only manager, admin and creator of the comment can delete this comment"
-            })
-           }
+
+            if(comment.user_id != user.id && user.role != "Admin" && user.role != "Manager"){return response.status(401).json({massage: "Only manager, admin and creator of the comment can delete this comment"})}
+
         }catch(e){
-            if(!comment["user_id"] == null){
-                return response.status(401).json({message: "Anauthorized"})
-            }
+
+            if(!comment.user_id == null){return response.status(401).json({message: "Anauthorized"})}
+
         }
-        if(comment["reply_to"] != null && comment["reply_to"] != ""){
-            const mother_comment = await Comment.find(comment["reply_to"])
-            if(!mother_comment){
-                return response.status(404).json({
-                    massage: "The replied comment not found"
-                })
-            }
+
+        if(comment.reply_to != null && comment.reply_to != ""){
+
+            const mother_comment = await Comment.find(comment.reply_to)
+
+            if(!mother_comment){return response.status(404).json({massage: "The replied comment not found"})}
+
             const partial_replies = mother_comment.replies.split(",")
             partial_replies.splice(partial_replies.indexOf(comment.id),1)
             mother_comment.replies = partial_replies.toString()
+
             try{
+
                 await mother_comment.save()
+
             }catch(e){
+
                 return response.status(404).json({massage: "Error saveing replied comment"})
+
             }
+
         }
         try{
+
            await comment.delete()
            return response.status(200).json({massage: "Comment deleted succesfully"})
+
         }catch(e){
-            return response.status(500).json({
-                massage: "Error deleting comment",
-                error: e
-            })
+
+            return response.status(500).json({massage: "Error deleting comment",error: e})
+
         }
 
     }
     async show({params, response}){
         try{
-        const comments = await Database.select("*").from("comments").where("post_id",params.id).where("status","Approved").where("reply_to",null).orderBy("created_at","desc")
-        let counter = comment.length
-        for(let comment of comments){
-            const replies = comment.replies ? comment.replies.split(",") : []
-            comment["replying_comments"] = []
-            for(let i=0;i<replies.length;i++){
-                const partial_comment = await Comment.find(replies[i])
-                if(partial_comment.status == "Approved"){
-                comment["replying_comments"].push(partial_comment)
-                counter++
+
+            const comments = await Database.select("*").from("comments").where("post_id",params.id).where("status","Approved").where("reply_to",null).orderBy("created_at","desc")
+            let counter = comment.length
+
+            for(let comment of comments){
+
+                const replies = comment.replies ? comment.replies.split(",") : []
+                comment.replying_comments = []
+
+                for(let i=0;i<replies.length;i++){
+
+                    const partial_comment = await Comment.find(replies[i])
+
+                    if(partial_comment.status == "Approved"){
+                    comment.replying_comments.push(partial_comment)
+                    counter++
+                    }
+
                 }
             }
-        }
-        return response.status(200).json({
-            massage: "Comments loaded successfully" ,
-            comments: comments,
-            comment_counts: counter
-        })
+
+        return response.status(200).json({massage: "Comments loaded successfully" ,comments: comments,comment_counts: counter})
+
         }catch(e){
-            return response.status(500).json({
-                massage: "Error loading comments",
-                error: e
-            })
+
+            return response.status(500).json({massage: "Error loading comments",error: e})
+
         }
+
     }
 }
 
